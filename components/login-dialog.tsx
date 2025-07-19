71506489ce2f7721d49ec2776b25d4e5d41f11ed
Han <hander.juan@gmail.com>
2025-07-19 13:34:01 +0200

    feat: add responsive login dialog

--- components/login-dialog.tsx
+++ components/login-dialog.tsx
@@ -1,13 +1,77 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { login, signup } from "@/lib/actions";
+import { useState } from "react";
+import { Button } from "./ui/button";
+
 export function LoginDialog() {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  if (isDesktop) {
+    return (
+      <Dialog onOpenChange={setOpen} open={open}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Login</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Login</DialogTitle>
+          </DialogHeader>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+  return (
+    <Drawer onOpenChange={setOpen} open={open}>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Login</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Login</DrawerTitle>
+        </DrawerHeader>
+        <LoginForm />
+        <DrawerFooter className="pt-2">
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+function LoginForm() {
   return (
     <form>
       <label htmlFor="email">Email:</label>
       <input id="email" name="email" required type="email" />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" required type="password" />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+      <button formAction={login} type="button">
+        Log in
+      </button>
+      <button formAction={signup} type="button">
+        Sign up
+      </button>
     </form>
   );
 }
